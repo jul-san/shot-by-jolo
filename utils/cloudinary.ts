12923e8532127e20445b55204b58e6a9c4ced841
Julian Sanchez <julianwsan@gmail.com>
2024-03-17 02:30:53 -0400

    sooo much

---
+++ utils/cloudinary.ts
@@ -0,0 +1,10 @@
+import cloudinary from "cloudinary";
+
+cloudinary.v2.config({
+  cloud_name: 'dw7rrh9ks',
+  api_key: '831766958659553',
+  api_secret: 'cmeJffQ7SzP6xzO81I1ZDMo6eLc',
+  secure: true,
+});
+
+export default cloudinary;
