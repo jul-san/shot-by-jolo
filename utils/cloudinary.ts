8d78c0c5fd28283af70c68990f0bd7808ef59ceb
Julian Sanchez <julianwsan@gmail.com>
2024-03-18 15:45:53 -0400

    figured out cloudinary

--- utils/cloudinary.ts
+++ utils/cloudinary.ts
@@ -1,9 +1,9 @@
 import cloudinary from "cloudinary";
 
 cloudinary.v2.config({
-  cloud_name: 'dw7rrh9ks',
-  api_key: '831766958659553',
-  api_secret: 'cmeJffQ7SzP6xzO81I1ZDMo6eLc',
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
 
